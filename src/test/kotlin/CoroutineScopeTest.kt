e3dc1b5028e64f1717f041e0a32de8e0d60fafe5
Fadhli Syaifullah <me@fadhli.dev>
2023-07-29 13:16:35 +0700

    coroutineScope function

--- src/test/kotlin/CoroutineScopeTest.kt
+++ src/test/kotlin/CoroutineScopeTest.kt
@@ -1,5 +1,6 @@
 import kotlinx.coroutines.*
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class CoroutineScopeTest {
 
@@ -42,6 +43,26 @@ class CoroutineScopeTest {
             // This will cancel the scope, so all coroutines running using this scope is also cancelled
             scope.cancel()
             joinAll(job1, job2)
+        }
+    }
+
+    @Test
+    fun `coroutine scope function test`() {
+        suspend fun <T> getValue(value: T): T {
+            delay(2_000)
+            return value
+        }
+
+        // Bundle simple coroutine processes without creating scope using CoroutineScope
+        suspend fun sum(n1: Int, n2: Int): Int = coroutineScope {
+            val x = async { getValue(n1) }
+            val y = async { getValue(n2) }
+            x.await() + y.await()
+        }
+
+        runBlocking {
+            val result = async { sum(100, 200) }
+            assertEquals(300, result.await())
         }
     }
 
